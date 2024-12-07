47dcc40d4a902553ee4c63e22719c4b071f2d73d
jossyfresh <jossyfresh2002@gmail.com>
2024-12-07 20:36:29 +0300

    Project initiation anf folder strucutre

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.10-slim
+
+WORKDIR /app
+
+# Copy the requirements file to the working directory
+COPY requirements.txt .
+
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the project files into the container
+COPY . .
+
+# Set the environment variable to prevent Python from buffering output
+ENV PYTHONUNBUFFERED=1
+
+# Run the bot
+CMD ["python", "main.py"]
---
+++ main.py
@@ -0,0 +1,13 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from config import API_TOKEN
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token("8125507956:AAFS5f-Hq92hrURGnck5eALNR0KG_CXeBm4").build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
